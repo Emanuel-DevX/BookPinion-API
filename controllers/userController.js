0b3baa78a21d774e68173ed5a2b47575b20ff055
Emanuel-DevX <jefymoxi@gmail.com>
2025-07-03 02:34:15 -0700

    Create registerUser controller for signups

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,21 @@
-const registerUser = async (req, res) => {};
+const User = require("../models/User");
+
+const registerUser = async (req, res) => {
+  const { username, password, firstName, lastName } = req.body;
+
+  try {
+    const existing = await User.findOne({ username });
+    if (existing) {
+      return res.status(400).json({ message: "Username already taken" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = User.create({
+      username,
+      password: hashedPassword,
+      firstName,
+      lastName,
+    });
+  } catch (error) {}
+};
 
 module.exports = { registerUser };
