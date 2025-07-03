8ba0e9845dd7f7da3564b2dbfe2f40c924c846a6
Emanuel-DevX <jefymoxi@gmail.com>
2025-07-03 02:05:31 -0700

    Define Review schema structure

---
+++ models/Review.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const reviewSchema = new mongoose.sSchema({
+  isbn: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: user,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true,
+  },
+  comment: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+const Review = mongoose.model("Review", reviewSchema);
+module.exports = Review;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 require("dotenv").config();
 const PORT = process.env.PORT;
+const connectDB = require("./config/db");
+
+connectDB();
 
 const app = express();
 
