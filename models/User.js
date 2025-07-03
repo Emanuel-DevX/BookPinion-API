cee5e35d3d5a0e3d64fbf4fa762a4962d86492df
Emanuel-DevX <jefymoxi@gmail.com>
2025-07-03 02:25:06 -0700

    Define User model schema structure

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 5,
+  },
+  passwword: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model(userSchema);
+module.exports(User);
--- server.js
+++ server.js
@@ -3,9 +3,13 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const connectDB = require("./config/db");
 
+const userRoutes = require("./routes/userRoutes");
+
 connectDB();
 
 const app = express();
+
+app.use("/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`All good surver is running on port: ${PORT}`);
