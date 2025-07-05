653bb0bbf43999b3ff806a9c422993dc2c7697e9
Emanuel-DevX <jefymoxi@gmail.com>
2025-07-04 18:17:19 -0700

    Add deleteUser controller with protected route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser, loginUser } = require("../controllers/userController");
+const {
+  registerUser,
+  loginUser,
+  deleteUser,
+} = require("../controllers/userController");
+const protect = require("../middlewares/authMiddleware");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.delete("/:id", protect, deleteUser);
 
 module.exports = router;
